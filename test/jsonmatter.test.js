1df4e5b760ecbcd6c455c1bbd7ca646fb41e2c91
hotoo <hotoo.cn@gmail.com>
2016-09-01 15:27:49 +0800

    feat(stream): json formatter by stream.
    fixed #2

---
+++ test/jsonmatter.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const tokenizer = require('json-tokenizer');
+const fs = require('fs');
+const path = require('path');
+require('should');
+const jsonmatter = require('..');
+
+describe('test/jsonmatter.test.js', function() {
+  it('online.json', function(done) {
+    let formattedJSON = '';
+    fs.createReadStream(path.resolve(__dirname, './fixtures/oneline.json'))
+      .pipe(tokenizer())
+      .pipe(jsonmatter())
+      .on('data', (chunk) => {
+        formattedJSON += String(chunk);
+      })
+      .on('end', function() {
+        const str = fs.readFileSync(path.resolve(__dirname, './result/oneline.json'), 'utf-8');
+        formattedJSON.should.be.eql(str);
+        done();
+      });
+  });
+});
