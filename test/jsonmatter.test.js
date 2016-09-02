2ccbac08d5975295643e993ddf5692a635bd858e
hotoo <hotoo.cn@gmail.com>
2016-09-02 23:02:13 +0800

    test(array): add array json test.

--- test/jsonmatter.test.js
+++ test/jsonmatter.test.js
@@ -21,4 +21,19 @@ describe('test/jsonmatter.test.js', function() {
         done();
       });
   });
+
+  it('array.json', function(done) {
+    let formattedJSON = '';
+    fs.createReadStream(path.resolve(__dirname, './fixtures/array.json'))
+      .pipe(tokenizer())
+      .pipe(jsonmatter())
+      .on('data', (chunk) => {
+        formattedJSON += String(chunk);
+      })
+      .on('end', function() {
+        const str = fs.readFileSync(path.resolve(__dirname, './result/array.json'), 'utf-8');
+        formattedJSON.should.be.eql(str);
+        done();
+      });
+  });
 });
